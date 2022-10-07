8cf6d61a4269dda72932d77ac0a1e5300e718c2d
smusman98 <smusman@objects.email>
2022-10-07 16:32:42 +0500

    PSMEL-173 - HTML Fix
    Div was not closed.

--- Postman/Postman-Configuration/PostmanConfigurationController.php
+++ Postman/Postman-Configuration/PostmanConfigurationController.php
@@ -264,6 +264,7 @@ class PostmanConfigurationController {
         do_settings_sections( PostmanSendinblueTransport::SENDINBLUE_AUTH_OPTIONS );
         print '</div>';
         print '<div id="postmark_settings" class="authentication_setting non-basic non-oauth2">';
+		print '</div>';
         do_settings_sections( PostmanPostmarkTransport::POSTMARK_AUTH_OPTIONS );
 		print '<div id="sparkpost_settings" class="authentication_setting non-basic non-oauth2">';
         do_settings_sections( PostmanSparkPostTransport::SPARKPOST_AUTH_OPTIONS );
