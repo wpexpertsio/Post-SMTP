9b5fb25465bea3a8a708bdaa8aa51bc8b0a0093b
smusman98 <smusman@objects.email>
2023-04-26 15:21:36 +0500

    PSMEL-325 - Conditional Notice

--- Postman/Postman-Send-Test-Email/PostmanSendTestEmailController.php
+++ Postman/Postman-Send-Test-Email/PostmanSendTestEmailController.php
@@ -189,11 +189,23 @@ class PostmanSendTestEmailController {
 		print '<section>';
 		printf( '<p><label>%s</label></p>', esc_html__( 'Status', 'post-smtp' ) );
 		print '<textarea id="postman_test_message_error_message" class="ps-textarea" readonly="readonly" cols="65" rows="4"></textarea>';
-		print '
-		<div class="ps-broken-mail-notice">
-			<span class="dashicons dashicons-info"></span> Is your email\'s Header or Body broken? <a href="https://postmansmtp.com/fix-for-broken-emails/" target="_blank">Learn how to fix</a>.
-		</div>
-		';
+		
+		$transport = PostmanOptions::getInstance()->getTransportType();
+
+		if( 
+			version_compare( '8.0.0', phpversion(), '<=' ) 
+			&&
+			( $transport == 'smtp' || $transport == 'default' )
+		) {
+
+			print '
+			<div class="ps-broken-mail-notice">
+				<span class="dashicons dashicons-info"></span> Is your email\'s Header or Body broken? <a href="https://postmansmtp.com/fix-for-broken-emails/" target="_blank">Learn how to fix</a>.
+			</div>
+			';
+
+		}  
+
 		print '</section>';
 		print '</fieldset>';
 
