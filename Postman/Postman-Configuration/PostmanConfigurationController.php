e3c1547d8a1f88ba580f23cbef3ce743a656ed0a
wpexpertsio <contact@wpexperts.io>
2023-05-29 11:48:18 +0500

    PSMEL-87 - Improved translation string.

--- Postman/Postman-Configuration/PostmanConfigurationController.php
+++ Postman/Postman-Configuration/PostmanConfigurationController.php
@@ -553,8 +553,9 @@ class PostmanConfigurationController {
 		print '<fieldset>';
 		printf( '<legend>%s</legend>', esc_html_x( 'Who is the mail coming from?', 'Wizard Step Title', 'post-smtp' ) );
 		printf( '<p>%s</p>', esc_html__( 'Enter the email address and name you\'d like to send mail as.', 'post-smtp' ) );
+		// translators: 1: Opening paragraph tag, 2: Emphasized "not", 3: Remaining sentence
 		printf( 
-			'<p>%s <em>%s</em> %s</p>', 
+			'<p>%1$s <em>%2$s</em> %3$s</p>', 
 			esc_html__( 'Please note that to prevent abuse, many email services will ', 'post-smtp' ), 
 			esc_html__( 'not', 'post-smtp' ), 
 			esc_html__( 'let you send from an email address other than the one you authenticate with.', 'post-smtp' ) 
