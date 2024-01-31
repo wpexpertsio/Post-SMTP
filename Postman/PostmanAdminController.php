c417a02dfd0e9efbbeb721332a6ade14d2cf1d50
FurqanFazal1 <furqan@objects.email>
2024-01-31 17:36:54 +0500

    PSMEL-411 - Delete report settings on reset

--- Postman/PostmanAdminController.php
+++ Postman/PostmanAdminController.php
@@ -363,6 +363,11 @@ if ( ! class_exists( 'PostmanAdminController' ) ) {
 
 				}
 
+				//delete postman health report settings on reset
+				delete_option( 'postman_rat' );
+				delete_option( '_transient_timeout_ps_rat_has_sent' );
+				delete_option( '_transient_ps_rat_has_sent' );
+
 				$this->messageHandler->addMessage( __( 'Plugin data was removed.', 'post-smtp' ) );
 
 				/**
