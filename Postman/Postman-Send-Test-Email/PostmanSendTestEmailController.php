8011f5dee236a1cf584e88ce0f79ee8601fb9ba1
Syed Muhammad Usman <smusman@objects.email>
2024-09-27 15:37:33 +0500

    PSMEL-738 - Typo Fix

--- Postman/Postman-Send-Test-Email/PostmanSendTestEmailController.php
+++ Postman/Postman-Send-Test-Email/PostmanSendTestEmailController.php
@@ -222,7 +222,7 @@ class PostmanSendTestEmailController {
 			'<p>%s <b>%s</b> %s</p>', 
 			esc_html__( 'This is the conversation between Postman and the mail server. It can be useful for diagnosing problems.', 'post-smtp' ) ,
 			esc_html__( 'DO NOT', 'post-smtp' ),
-			esc_html__( 'post it on-line, it may contain your account password.', 'post-smtp' )
+			esc_html__( 'post it online, it may contain your account password.', 'post-smtp' )
 		);
 		print '<section>';
 		printf( '<p><label for="postman_test_message_transcript">%s</label></p>', esc_html__( 'Session Transcript', 'post-smtp' ) );
