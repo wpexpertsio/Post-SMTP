03379935ed8f2a002d66a82304a5ab7779b0d010
ahsan.amin@objects.email <ahsan.amin@objects.email>
2025-03-05 15:05:15 +0500

    SMTPSUPORT-420 - Excaping Url Issue

--- Postman/Extensions/Core/Notifications/PostmanNotify.php
+++ Postman/Extensions/Core/Notifications/PostmanNotify.php
@@ -90,7 +90,7 @@ class PostmanNotify {
             foreach ( $_POST['postman_options']['webhook_alerts_urls'] as $key => $url ) {
 
                 if( ! empty( $url ) ) {
-                    $webhook_urls[] = esc_url( $url );
+                    $webhook_urls[] = esc_url_raw( $url );
                 }
     
             }
